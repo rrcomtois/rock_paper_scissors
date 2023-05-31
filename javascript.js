c8d639a676c1ed494c3bedc2a924d6020a6fcb65
Rob Comtois <rrcomtois@gmail.com>
2023-05-31 09:20:26 -0400

    added game score checks, reset button, polish

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 const choices = ["rock", "paper", "scissors"]
+const gameLimit = 2;
 let playerScore = 0;
 let computerScore = 0;
+let round = "";
+let scoring = "";
+
 
 const buttons = document.querySelectorAll('button');
-const result = document.getElementById('result');
+const resultText = document.getElementById('result');
 const playerScoreText = document.getElementById('playerScore');
 const computerScoreText = document.getElementById('computerScore');
-
+const gameText = document.getElementById('game');
+const container = document.getElementById('container');
+const reset = document.getElementById('reset');
 
 
 function getComputerChoice() {
     return choices[Math.floor((Math.random() * 3))];
 }
 
-function playRound(playerSelection, computerSelection){
+function choiceCompare(playerSelection, computerSelection){
     if (playerSelection == "") return "Round cancelled.";
     return (playerSelection == computerSelection) ? `${playerSelection} against ${computerSelection} results in a tie!` :
     (playerSelection == "rock") ? (computerSelection == "paper") ? `${playerSelection} beats ${computerSelection}, you win!` : `${computerSelection} beats ${playerSelection}, you lose!` :
@@ -21,19 +27,47 @@ function playRound(playerSelection, computerSelection){
     (computerSelection == "paper") ? `${playerSelection} beats ${computerSelection}, you win!` : `${computerSelection} beats ${playerSelection}, you lose!`;
 }
 
+function playRound(playerSelection) {
+    if (playerScore == gameLimit || computerScore == gameLimit) return;
+    round = choiceCompare(playerSelection, getComputerChoice());
+    resultText.innerHTML = round;
+    scoring = round.slice(-5).trim();
+    if (scoring == "win!") {
+        playerScore++;
+        playerScoreText.innerHTML = `Player score: ${playerScore}`;
+    }
+    else if (scoring == "lose!") {
+        computerScore++;
+        computerScoreText.innerHTML = `Computer score: ${computerScore}`;
+    }
+    scoreCheck();
+}
+
+function scoreCheck() {
+    if (playerScore == gameLimit){
+        gameText.innerHTML = "You won the series!";
+        reset.hidden = false;
+    }
+    else if (computerScore == gameLimit){
+        gameText.innerHTML = "Ooo, looks like the computer won the series... better luck next time!";
+        reset.hidden = false;
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gameText.innerHTML = "";
+    playerScoreText.innerHTML = `Player score: ${playerScore}`;
+    computerScoreText.innerHTML = `Computer score: ${computerScore}`;
+    resultText.innerHTML = "Click a button to make your selection";
+    reset.hidden = true;
+}
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', function () {
-        let round = playRound(this.textContent.toLowerCase(), getComputerChoice());
-        result.innerHTML = round;
-        let scoring = round.slice(-5).trim();
-        if (scoring == "win!") {
-            playerScore++;
-            playerScoreText.innerHTML = `Player Score: ${playerScore}`;
-        }
-        else if (scoring == "lose!") {
-            computerScore++;
-            computerScoreText.innerHTML = `Computer Score: ${computerScore}`;
-        }
+        if (this.id != "reset") playRound(this.textContent.toLowerCase());
+        else resetGame();
     });
 });
